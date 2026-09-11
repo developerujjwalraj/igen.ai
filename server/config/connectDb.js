ec1b93bd894d40962baaa4d6fa91a1665dc42414
Ujjwal Raj <ujjwalraj234dz@gmail.com>
2026-09-11 08:56:29 +0530

    Fix Windows DNS SRV ECONNREFUSED for MongoDB Atlas in connectDb

--- server/config/connectDb.js
+++ server/config/connectDb.js
@@ -2,7 +2,16 @@ import mongoose from "mongoose";
 
 const connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL)
+        let url = process.env.MONGODB_URL;
+        if (url && url.includes("cluster0.e1emzih.mongodb.net")) {
+            const authMatch = url.match(/mongodb\+srv:\/\/([^@]+)@cluster0\.e1emzih\.mongodb\.net\/?([^?]*)/);
+            if (authMatch) {
+                const creds = authMatch[1];
+                const db = authMatch[2] ? authMatch[2].split("?")[0] : "Agent";
+                url = `mongodb://${creds}@ac-rzkwh6g-shard-00-00.e1emzih.mongodb.net:27017,ac-rzkwh6g-shard-00-01.e1emzih.mongodb.net:27017,ac-rzkwh6g-shard-00-02.e1emzih.mongodb.net:27017/${db}?ssl=true&replicaSet=atlas-fckf1q-shard-0&authSource=admin&retryWrites=true&w=majority`;
+            }
+        }
+        await mongoose.connect(url)
         console.log("DataBase Connected")
     } catch (error) {
         console.log(`DataBase Error ${error}`)
